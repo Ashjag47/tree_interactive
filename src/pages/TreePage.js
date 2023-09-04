61c07e943a4ceb814a979661046451ac87106fa3
Ashjag47 <ashjagmk@gmail.com>
2023-09-04 08:53:44 +0530

    Feat: add a tree like structure ui with mock data.

--- src/pages/TreePage.js
+++ src/pages/TreePage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Tree from "react-d3-tree";
+import { Box } from "@chakra-ui/react";
 
 function TreePage() {
+  const [SCB, setSCB] = useState({
+    name: "SCB",
+    children: [{ name: "SCB1" }, { name: "SCB2" }],
+  });
   return (
-    <div>
-      <h1>TreePage</h1>
-    </div>
+    <Box h="100vh" w="100vw">
+      <Tree data={SCB} />
+    </Box>
   );
 }
 
